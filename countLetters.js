fa2be7a0bf4ae08d5459c73851dea6dbe80ee757
EVillett <emily.villett@gmail.com>
2022-09-21 16:02:44 -0700

    adding countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const countLetters = function(str) {
+  const results = {};
+
+  for (const letter of str) {
+    if (letter === ' ') {
+      continue;
+    }
+
+    if (!results[letter]) {
+      results[letter] = 0;
+    }
+
+
+    results[letter]++;
+  }
+
+  return results;
+};
+
+console.log(countLetters("Lighthouse in the house"));
