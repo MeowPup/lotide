f0052be800951dace141b921b3f018a5bdbdf8c5
EVillett <emily.villett@gmail.com>
2022-09-21 16:51:21 -0700

    adding letter positions to git

---
+++ letterPositions.js
@@ -0,0 +1,15 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === " ") {
+      continue;
+    } else if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("Lighthouse in the house"));
