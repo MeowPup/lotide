2647edd459e48a115465f9d8cd29e771d17b6f7c
EVillett <emily.villett@gmail.com>
2022-09-26 15:49:01 -0700

    updating middleTest to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -5,19 +5,19 @@ const middle = require('../middle');
 
 describe("#middle", () => {
   it('should return [2]', () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2, 3]), [2]));
+    assert.deepEqual(assertArraysEqual(middle([1, 2, 3]), [2]));
   });
 
   it('should return [3]', () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
+    assert.deepEqual(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
   });
 
   it('should return []', () => {
-    assert.strictEqual(assertArraysEqual(middle([1]), []));
+    assert.deepEqual(assertArraysEqual(middle([1]), []));
   });
 
   it('should return [2, 3]', () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
+    assert.deepEqual(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
   });
 
 });
